de8437867e539d699a64ba6bdabe8eac8d097d05
rahulhaldkar1 <rahulhaldkar444@gmail.com>
2025-03-11 14:22:07 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.sql.Date;
+
+public class Main {
+    public static void main(String[] args)  
+    {
+        System.out.println("This is Java Program");
+        System.out.println("Today is Monday");
+        System.out.println(new Date(0));
+
+    }
+
+}
