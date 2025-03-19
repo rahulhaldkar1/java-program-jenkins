2a82c74676a036c51975db9e7c8a09a5a37e3c89
rahulhaldkar1 <rahulhaldkar444@gmail.com>
2025-03-19 15:06:42 +0530

    updated

--- Student.java
+++ Student.java
@@ -2,10 +2,9 @@ public class Student {
 
     public Student(){
         System.out.println("Creating student object");
-
         }
         public void sayHello(){
-        System.out.println("i am student");
+        System.out.println("Hi , i am student in global");
         }
     }
     
